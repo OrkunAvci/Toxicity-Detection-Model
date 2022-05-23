c74943794a9bce8ea5fdd0641c6245853db514a8
OrkunAvci <orkunavci28@gmail.com>
2022-05-23 16:14:03 +0300

    Collect live data

---
+++ get_comments.py
@@ -0,0 +1,21 @@
+from api.youtube import Youtube
+import json
+
+video_ids = []
+comments = {}
+
+with open("./data/video_ids.json", "r") as f:
+	video_ids = json.loads(f.read())
+
+with open("./data/comments.json", "r") as f:
+	comments = json.loads(f.read())
+
+yt = Youtube()
+for id in video_ids:
+	if id not in comments.keys():
+		print("Session ID: ", id)
+		comments[id] = yt.get_comments(id)
+		with open("./data/comments.json", "r+") as f:
+			f.write(json.dumps(comments))
+	else:
+		print("Skipped ", id)
