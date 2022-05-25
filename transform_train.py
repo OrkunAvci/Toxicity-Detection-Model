80e7b172b4e299c45e9c6028421b96bef4abce39
OrkunAvci <orkunavci28@gmail.com>
2022-05-25 06:14:05 +0300

    Better logic, still brute force save

--- transform_train.py
+++ transform_train.py
@@ -4,9 +4,15 @@ import pandas as pd
 df = pd.read_csv("./data/train.csv")
 out = pd.read_csv("./data/new_train.csv")
 
+logged = list(df.loc[:,"comment_text"].array)
+processed = list(out.loc[:,"Comment"].array)
+
+to_process = [comment for comment in logged if comment not in processed]
+
 detector = Detector()
-for text in df.loc[ len(out.loc[:,"Comment"].array) : ,"comment_text"].array:
+for text in to_process:
 	detection = detector.detect(text)
 	if detection:
-		out.loc[out.shape[0]] = dict(zip(out.columns, detection))
+		created = pd.DataFrame(data=[detection], columns=out.columns)
+		out = pd.concat([out, created], ignore_index=True)
 		out.to_csv("./data/new_train.csv", index=False)
