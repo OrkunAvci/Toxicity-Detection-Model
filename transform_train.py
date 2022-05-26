8734346e5d99b4d050b318e172a213cc4831f2a0
OrkunAvci <orkunavci28@gmail.com>
2022-05-26 14:29:51 +0300

    Optimize and lock saving process

--- transform_train.py
+++ transform_train.py
@@ -1,5 +1,9 @@
 from api.perspective import Detector
 import pandas as pd
+import threading
+
+CHUNK_SIZE = 1000
+lock = threading.Lock()
 
 df = pd.read_csv("./data/train.csv")
 out = pd.read_csv("./data/new_train.csv")
@@ -9,10 +13,17 @@ processed = list(out.loc[:,"Comment"].array)
 
 to_process = [comment for comment in logged if comment not in processed]
 
+def chunks(seq, size):
+    return list(seq[pos:pos + size] for pos in range(0, len(seq), size))
+
 detector = Detector()
-for text in to_process:
-	detection = detector.detect(text)
-	if detection:
-		created = pd.DataFrame(data=[detection], columns=out.columns)
-		out = pd.concat([out, created], ignore_index=True)
+for chunk in chunks(to_process, CHUNK_SIZE):
+	detections = []
+	for text in chunk:
+		detection = detector.detect(text)
+		if detection:
+			detections.append(detection)
+	created = pd.DataFrame(data=detections, columns=out.columns)
+	out = pd.concat([out, created], ignore_index=True)
+	with lock:
 		out.to_csv("./data/new_train.csv", index=False)
